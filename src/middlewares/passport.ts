f2147a332fae364043c35dea604963f7f28255f3
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-11-20 22:11:24 +0100

    fix git ignore

--- src/middlewares/passport.ts
+++ src/middlewares/passport.ts
@@ -4,14 +4,12 @@ import config from '../config/config';
 // https://medium.com/javascript-in-plain-english/creating-a-rest-api-with-jwt-authentication-and-role-based-authorization-using-typescript-fbfa3cab22a4
 export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
     //Get the jwt token from the head
-    const token: string | string[] | undefined = req.headers['auth'];
+    const token = <string>req.headers['auth'];
     let jwtPayload;
 
     //Try to validate the token and get data
     try {
-        if (token == 'string') {
-            jwtPayload = jwt.verify(token, config.jwtSecret);
-        }
+        jwtPayload = jwt.verify(token, config.jwtSecret);
         res.locals.jwtPayload = jwtPayload;
     } catch (error) {
         //If token is not valid, respond with 401 (unauthorized)
@@ -21,7 +19,7 @@ export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
 
     //The token is valid for 1 hour
     //We want to send a new token on every request
-    // @ts-ignore
+
     const { email, nickname } = jwtPayload;
     const newToken = jwt.sign({ email, nickname }, config.jwtSecret, {
         expiresIn: '1h',
