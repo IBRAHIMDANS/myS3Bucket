df42d247812c827af089c2b867f1cd9c53ada7ca
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-11-20 22:14:08 +0100

    prob any passport

--- src/middlewares/passport.ts
+++ src/middlewares/passport.ts
@@ -19,7 +19,7 @@ export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
 
     //The token is valid for 1 hour
     //We want to send a new token on every request
-//change any  afta
+    //change any  afta
     const { email, nickname }: any = jwtPayload;
     const newToken = jwt.sign({ email, nickname }, config.jwtSecret, {
         expiresIn: '1h',
