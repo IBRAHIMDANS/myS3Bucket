13b7d92cf586fa0e954c803ddaa3d4dae1dff1b1
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-12-10 10:07:09 +0100

    fix test

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,35 +1,32 @@
-import { app, port } from '../src/main';
-import { expect } from 'chai';
+import { app } from '../src/main';
 import { Connection, createConnection } from 'typeorm';
 import supertest from 'supertest';
 
-// const server = supertest(app);
-//
-// let connection: Connection;
+const server: supertest.SuperTest<supertest.Test> = supertest(app);
+let connection: Connection;
+let port: import('http').Server;
+afterAll(async done => {
+    await connection.close();
+    await port.close();
+    return done();
+});
 
-// afterAll(async done => {
-//     await connection.close();
-//     return done();
-// });
-//
-// beforeAll(async done => {
-//     // Step 01: Drop database
-//     connection = await createConnection();
-//     await connection.dropDatabase();
-//     await connection.close();
-//     done();
-// });
+beforeAll(async done => {
+    // Step 01: Drop database
+    port = app.listen(8082);
+    connection = await createConnection('Test');
+    done();
+});
 describe('Index Test', () => {
     it('Index', function() {
-        expect(true).to.equal(true);
+        expect(true).toEqual(true);
     });
 });
-describe('Get', () => {
+describe('Get index', () => {
     it('api', async done => {
         const res = await server.get('/api');
-        console.log('res');
         console.log(res.status);
-        expect(res.status).to.be('string', '200');
+        expect(res.status).toBe(200);
         done();
     });
 });
